efb449dc4521a1d794d25603b586999ec702a893
joshuat-23 <124537595+joshuat-23@users.noreply.github.com>
2023-02-06 08:22:51 +0800

    Add files via upload

---
+++ Year-11-CS-Unit-6-Classes-Tutorial-Cube/src/Cube.java
@@ -0,0 +1,44 @@
+public class Cube {
+    private int side;
+
+    public Cube(){
+        side = 1;
+    }
+
+    public Cube(int length){
+        if(length<1){
+            throw new IllegalArgumentException("A cube’s side length cannot be less than 1!");
+        }
+        else{
+            side = length;
+        }
+
+    }
+
+    public int getSide(){
+        return side;
+    }
+
+    public void setSide(int side){
+        if(side<1){
+            throw new IllegalArgumentException("A cube’s side length cannot be less than 1!");
+        }
+        else{
+            this.side = side;
+        }
+    }
+
+    public int getSurfaceArea(){
+
+        return 6*side*side;
+    }
+
+    public int getVolume(){
+
+        return side*side*side;
+    }
+
+    public String toString(){
+        return "Cube{side=" + side + "}";
+    }
+}
